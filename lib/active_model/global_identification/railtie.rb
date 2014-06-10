4f258efee906387e1a2062f76fc03508386ab3a5
Samuel Cochran <sj26@sj26.com>
2014-06-10 11:52:23 +1000

    Don't collide with active_model/railtie

--- lib/active_model/global_identification/railtie.rb
+++ lib/active_model/global_identification/railtie.rb
@@ -1,7 +1,7 @@
 require 'rails/railtie'
 require 'active_model/global_identification'
 
-module ActiveModel
+module ActiveModel::GlobalIdentification
   # = Active Model GlobalID Railtie
   class Railtie < Rails::Railtie # :nodoc:
     initializer "active_model.globalid" do
